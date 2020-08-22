fdf32cc3095da506c7f8d8726a89daa64a2cb9b4
ls-d <lucyng_07@yahoo.es>
2020-08-22 05:53:38 -0500

    guardar, editar, eliminar y connect-flash

--- database/db.sql
+++ database/db.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE db_qlio;
+CREATE DATABASE belrvuwwsyvuuhi8mpl1;
 
-USE db_qlio;
+USE belrvuwwsyvuuhi8mpl1;
 
 -- USERS TABLE
 CREATE TABLE users(
@@ -14,18 +14,17 @@ ALTER TABLE users
     ADD PRIMARY KEY (id);
 
 ALTER TABLE users
-    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+    ALTER COLUMN id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 DESCRIBE users;
 
--- CONTACT TABLE
+-- CONTACTOS TABLE
 CREATE TABLE contactos(
     id INT(11) NOT NULL,
     nombre VARCHAR(150) NOT NULL,
     correo VARCHAR(150) NOT NULL,
     telefono VARCHAR(20) NOT NULL,
     mensaje VARCHAR(600) NOT NULL,
-    description TEXT,
     user_id INT(11),
     created_at timestamp NOT NULL DEFAULT current_timestamp,
     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
@@ -34,7 +33,7 @@ CREATE TABLE contactos(
 ALTER TABLE contactos
     ADD PRIMARY KEY (id);
 
-ALTER TABLE contantos
-    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+ALTER TABLE contactos
+    ALTER COLUMN id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 DESCRIBE contactos;
--- src/routes/contactos.js
+++ src/routes/contactos.js
@@ -7,7 +7,7 @@ router.get('/add', (req, res) => {
     res.render('contactos/add');
 });
 
-router.post('/add', (req, res) => {
+router.post('/add', async (req, res) => {
     const { nombre, correo, telefono, mensaje } = req.body;
     const newcontacto = {
         nombre,
@@ -15,8 +15,41 @@ router.post('/add', (req, res) => {
         telefono,
         mensaje
     };
-    console.log(newcontacto);
-    res.send('received');
+    await pool.query('INSERT INTO contactos set ?', [newcontacto]);
+    req.flash('success', 'Mensaje guardado satisfactoriamente');
+    res.redirect('/contactos');
 });
 
-module.exports =router;
+router.get('/', async (req, res) => {
+    const contactos = await pool.query('SELECT * FROM contactos');    
+    res.render('contactos/list', {contactos});
+});
+
+router.get('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM contactos WHERE ID = ?', [id]);
+    req.flash('success', 'Mensaje eliminado satisfactoriamente');
+    res.redirect('/contactos');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const contactos = await pool.query('SELECT * FROM contactos WHERE id = ?', [id]);
+    res.render('contactos/edit', {contacto: contactos [0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { nombre, correo, telefono, mensaje } = req.body;
+    const newContacto = {
+        nombre,
+        correo,
+        telefono,
+        mensaje
+    };
+    await pool.query('UPDATE contactos set ? WHERE id = ?', [newContacto, id]);
+    req.flash('success', 'Mensaje actualizado satisfactoriamente');
+    res.redirect('/contactos');
+});
+
+module.exports = router;
